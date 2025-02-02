e3dabc2aae8b6d11e2d11d431f3944b75ce5f365
Tushar Tari <tushartari11@gmail.com>
2025-02-02 19:46:25 +0100

    Data Binding changes

--- src/app/admin/edit-user/edit-user.component.ts
+++ src/app/admin/edit-user/edit-user.component.ts
@@ -11,4 +11,22 @@ export class EditUserComponent {
   adminName: string = 'Tushar';
   anchorUrl: string = 'http://google.com';
   disabled: boolean = true;
+  className: string = 'c1';
+
+  buttonClicked() {
+    console.log('Button Clicked...');
+    this.className = this.className === 'c1' ? 'c2' : 'c1';
+    this.disabled = !this.disabled;
+  }
+  captureValue(event: any) {
+    console.log('captureValue: ' + event.key);
+  }
+
+  onMouseOver(event: any) {
+    this.className = this.className === 'c1' ? 'c2' : 'c1';
+  }
+
+  onMouseOut(event: any) {
+    this.className = this.className === 'c2' ? 'c1' : 'c2';
+  }
 }
